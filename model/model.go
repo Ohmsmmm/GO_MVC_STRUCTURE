aea9962e9b3f12e39bf2d4e01e7c62f452082551
SakuBoyz <Ohmsm0150@gmail.com>
2020-10-09 01:27:17 +0700

    finish1.0

---
+++ model/model.go
@@ -0,0 +1,10 @@
+package model
+
+type DataCleansingRequest struct {
+	InputString string `json:"input_string"`
+}
+
+type DataCleansingResponse struct {
+	Word  string `json:"word"`
+	Count int    `json:"count"`
+}
--- server.go
+++ server.go
@@ -1,14 +1,20 @@
-package GO_MVC_STRUCTURE
+package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/GO_MVC_STRUCTURE/controller"
+	"github.com/gin-gonic/gin"
+)
 
 
 func main() {
+	r := setupRouter()
+	r.Run()
+}
+
+func setupRouter() *gin.Engine {
 	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
-	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+	data := controller.DataHandler{}
+	r.GET("/ping", controller.Ping)
+	r.POST("/DataCleansing/:input_string", data.EndPointDataCleansing)
+	return r
 }
